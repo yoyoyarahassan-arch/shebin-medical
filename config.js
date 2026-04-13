e7a1dd159a66462a1f97a24afcd5cccf4a0eff18
yoyoyarahassan-arch <yoyo.yara.hassan@gmail.com>
2026-04-14 01:03:42 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: 'https://zuuikhlfeluiyehlpuvk.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1dWlraGxmZWx1aXllaGxwdXZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwMjI3NzMsImV4cCI6MjA5MTU5ODc3M30.yhwCCpwz__WqIfCiNqq-a2Hos6Jibn51juq72X5x9HY'
+};
