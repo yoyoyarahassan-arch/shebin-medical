260fde1ee31e41d5d16c905f192adc6e295d698f
yoyoyarahassan-arch <yoyo.yara.hassan@gmail.com>
2026-04-14 01:10:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@
   var CONFIG = {
     dataSource: 'supabase',
     supabase: {
-      url: 'https://zuuikhlfeluiyehlpuvk.supabase.co',
-      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1dWlraGxmZWx1aXllaGxwdXZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwMjI3NzMsImV4cCI6MjA5MTU5ODc3M30.yhwCCpwz__WqIfCiNqq-a2Hos6Jibn51juq72X5x9HY',
+      url: window.SUPABASE_CONFIG.url,
+anonKey: window.SUPABASE_CONFIG.anonKey,
       tableDoctors: 'doctors',
       tableSpecialties: 'specialties'
     },
